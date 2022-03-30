4aa07e5332502ab34c07255d0976087f9ead1d59
Jikkou9 <72326079+Jikkou9@users.noreply.github.com>
2022-03-30 17:40:12 -0500

    Test Change

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			//Just a comment
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
