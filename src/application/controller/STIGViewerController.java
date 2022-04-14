baa7475dafcbff23bcdd7b663e29e17cfadc7442
Jikkou9 <72326079+Jikkou9@users.noreply.github.com>
2022-04-14 03:04:47 -0500

    Updated STIGViewer fxml to display scroll bar properly

--- src/application/controller/STIGViewerController.java
+++ src/application/controller/STIGViewerController.java
@@ -1,11 +1,9 @@
 package application.controller;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
 import javax.xml.parsers.ParserConfigurationException;
-
 import org.xml.sax.SAXException;
-
 import application.model.STIGDocument;
 import application.model.STIGRule;
 import javafx.collections.FXCollections;
@@ -20,17 +18,19 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class STIGViewerController {
 	
 	STIGDocument referenceSTIG;
-	STIGDocument filteredSTIG;
+	ArrayList<STIGRule> filteredSTIGRules;
 	ObservableList<STIGRule> stigRuleList = FXCollections.observableArrayList();
 
 	@FXML
@@ -89,10 +89,10 @@ public class STIGViewerController {
     // STIG Rule Content
     
     @FXML
-    private TextArea STIGRuleHeader;
+    private TextFlow STIGRuleHeader;
 
     @FXML
-    private TextArea STIGRuleContent;
+    private TextFlow STIGRuleContent;
 
     
     
@@ -104,6 +104,88 @@ public class STIGViewerController {
     @FXML
     void DeleteFilter(ActionEvent event) {
 
+    }
+    
+    @FXML
+    void displaySelectedRule(MouseEvent event) {
+    	// Ensure that the table was double clicked to change the selected rule
+    	if( event.getClickCount() == 2 ) {
+    		// Get selected STIGRule
+    		STIGRule selectedRule = STIGRuleTable.getSelectionModel().getSelectedItem();
+    		
+    		// Clear the previous content
+    		STIGRuleHeader.getChildren().clear();
+    		STIGRuleContent.getChildren().clear();
+    		
+    		// Generate the Header
+    		// Header line 1
+    		Text tempTitle = new Text(referenceSTIG.getStigTitle() + "\n");
+    		tempTitle.setStyle("-fx-font-weight: bold");
+    		
+    		// Header line 2
+    		Text vulIDHeader = new Text("Vul ID: ");
+    		vulIDHeader.setStyle("-fx-font-weight: bold");
+    		Text vulIDContent = new Text(selectedRule.getVulID());
+    		
+    		Text ruleIDHeader = new Text("\tRule ID: ");
+    		ruleIDHeader.setStyle("-fx-font-weight: bold");
+    		Text ruleIDContent = new Text(selectedRule.getSubVulID());
+    		
+    		Text stigIDHeader = new Text("\tSTIG ID: ");
+    		stigIDHeader.setStyle("-fx-font-weight: bold");
+    		Text stigIDContent = new Text(selectedRule.getStigID() + "\n");
+    		
+    		// Header line 2
+    		Text severityHeader = new Text("Severity: ");
+    		severityHeader.setStyle("-fx-font-weight: bold");
+    		Text severityContent = new Text(selectedRule.getSeverityCat());
+    		
+    		Text legacyIDsHeader = new Text("\tLegacy IDs: ");
+    		legacyIDsHeader.setStyle("-fx-font-weight: bold");
+    		Text legacyIDsContent = new Text(selectedRule.getLegacyIDs().toString().replaceAll("[\\[\\]]", "") + "\n");
+    		
+    		// Generate the Content
+    		// Group Title
+    		Text groupTitleHeader = new Text("Group Title: ");
+    		groupTitleHeader.setStyle("-fx-font-weight: bold");
+    		Text groupTitleContent = new Text(selectedRule.getGroupTitle() + "\n");
+    		
+    		// Rule Title
+    		Text ruleTitleHeader = new Text("Rule Title: ");
+    		ruleTitleHeader.setStyle("-fx-font-weight: bold");
+    		Text ruleTitleContent = new Text(selectedRule.getRuleTitle() + "\n");
+    		
+    		// Discussion
+    		Text discussionHeader = new Text("Discussion: ");
+    		discussionHeader.setStyle("-fx-font-weight: bold");
+    		Text discussionContent = new Text(selectedRule.getRuleDiscussion() + "\n");
+    		
+    		// Check Text
+    		Text checkTextHeader = new Text("Check Text: ");
+    		checkTextHeader.setStyle("-fx-font-weight: bold");
+    		Text checkTextContent = new Text(selectedRule.getCheckText() + "\n");
+    		
+    		// Fix Text
+    		Text fixTextHeader = new Text("Fix Text: ");
+    		fixTextHeader.setStyle("-fx-font-weight: bold");
+    		Text fixTextContent = new Text(selectedRule.getFixText() + "\n");
+    		
+    		// CCI
+    		
+    		STIGRuleHeader.getChildren().addAll(tempTitle, 
+    											vulIDHeader, vulIDContent, ruleIDHeader, ruleIDContent, stigIDHeader, stigIDContent,
+    											severityHeader, severityContent, legacyIDsHeader, legacyIDsContent);
+    		STIGRuleHeader.setStyle("-fx-font-size: 14px");
+    		STIGRuleContent.getChildren().addAll(groupTitleHeader, groupTitleContent,
+    											ruleTitleHeader, ruleTitleContent,
+    											discussionHeader, discussionContent,
+    											checkTextHeader, checkTextContent,
+    											fixTextHeader, fixTextContent);
+    		STIGRuleContent.setStyle("-fx-font-size: 14px");
+    		
+    		
+    		
+    	}
     }
 
     @FXML
@@ -156,16 +238,11 @@ public class STIGViewerController {
 		window.setTitle("Local Catalog");
 		window.show();
     }
-
-    @FXML
-    void loadSTIGRule(ActionEvent event) {
-
-    }
     
     void initializeSTIGViewer( String stigFileName ) throws ParserConfigurationException, SAXException, IOException {
     	// Populate the referenceSTIG and the filteredSTIG (filtered STIG is initialized with no filter in place)
     	referenceSTIG = new STIGDocument( stigFileName );
-    	filteredSTIG = referenceSTIG;
+    	filteredSTIGRules = referenceSTIG.getStigRuleArrayList();
     	
     	// Update Filtered STIG Table
     	updateFilteredSTIGTable();
@@ -177,7 +254,7 @@ public class STIGViewerController {
         STIGIDColumn.setCellValueFactory(new PropertyValueFactory<STIGRule, String>("stigID"));
         RuleIDColumn.setCellValueFactory(new PropertyValueFactory<STIGRule, String>("subVulID"));
         RuleNameColumn.setCellValueFactory(new PropertyValueFactory<STIGRule, String>("groupTitle"));
-    	STIGRuleTable.getItems().setAll( filteredSTIG.getStigRuleArrayList() );
+    	STIGRuleTable.getItems().setAll( filteredSTIGRules );
     }
 
 }
