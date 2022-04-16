117b21c0255d149c33448492c30834466d4f3cc2
phosu7 <99696023+phosu7@users.noreply.github.com>
2022-04-16 13:09:10 -0500

    java comment docs

--- src/application/controller/LocalCatalogController.java
+++ src/application/controller/LocalCatalogController.java
@@ -23,6 +23,10 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+/** This class is the controller of the Local catalog. It controls all the buttons and the list views to allow the user to update, delete and view the STIGs.
+ * @author Brandon Chung, Leon Le
+ *
+ */
 public class LocalCatalogController implements Initializable{
 	/* The LocalCatalogController is the controller for the LocalCatalog.
 	 * It provides functionality to browse your own local folders to upload, delete and update .xml files. */
@@ -34,6 +38,12 @@ public class LocalCatalogController implements Initializable{
     
     /* This method allows you to go to the STIGView once you select a certain .xml file on the list view and click on the view button.
      * It throws an exception if there is an issue loading a file in.*/
+    /**
+     * @param event , This event is from clicking on the button to go to switch the view to the STIG viewer.
+     * @throws IOException , when there is an issue loading the STIG file.
+     * @throws ParserConfigurationException, when there is an issue with configuration.
+     * @throws SAXException, when there is an issue with the XML parser or the application.
+     */
     @FXML
     void GoToSTIGViewer(ActionEvent event) throws IOException, ParserConfigurationException, SAXException {
     	if(STIGCatalogListView.getSelectionModel().getSelectedItem() != null) {
@@ -58,6 +68,10 @@ public class LocalCatalogController implements Initializable{
     }
     
     /* When you're on the local catalog, the main menu button when pressed will take you abck to the main menu.*/
+    /**
+     * @param event , Even is from clicking the button to go back to the main menu.
+     * @throws IOException throws an exception when there is problem loading in the fxml file.
+     */
     @FXML
     void GoToMainMenu(ActionEvent event) throws IOException {
     	// Load the Main Menu fxml file
@@ -73,6 +87,10 @@ public class LocalCatalogController implements Initializable{
     }
     
     /* Uploads the STIG file and updates the arraylist and listview to add the xml file.*/
+    /**
+     * @param event takes in the event when the upload button is pressed.
+     * @throws IOException throws an exception if there is an issue loading in the STIG.
+     */
     @FXML
     void UploadSTIG(ActionEvent event) throws IOException {
     	LocalCatalog.uploadXML(STIGCatalogListView);
@@ -80,6 +98,9 @@ public class LocalCatalogController implements Initializable{
     }
     
     /* Deletes the STIG file that is highlighted on the list view and calls upon the uploadSTIGCatalog to update the listView and arrraylist.*/
+    /**
+     * @param event takes in the event when the delete button is pressed.
+     */
     @FXML
     public void DeleteSTIG(ActionEvent event) {
     	if (STIGCatalogListView.getSelectionModel().getSelectedItem() != null) {
--- src/application/model/LocalCatalog.java
+++ src/application/model/LocalCatalog.java
@@ -12,7 +12,12 @@ import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
+/**This class is in charge of the Local Catalog section of the STIG viewer. It allows the users to upload, delete and view STIGS .xml files that are on their local directory.
+ * @author Brandon Chung, Leon Le
+ *
+ */
 public class LocalCatalog {
+	
 	
 	// declaring and intiializing arrays and making them global to call when needed for changes.
 	public static ArrayList<String> arrayOfSTIGS = new ArrayList<String>();
@@ -20,6 +25,9 @@ public class LocalCatalog {
 	public static String catalogPath = "src/data/catalog/";
 	
 	// reads and calls on to enumerate to create that arraylist as well. Prints out the arrayList in the console and returns the array of STIGS.
+	/**
+	 * @return , reutns teh array of stigs after reading in the catalogPath and creating an arraylist of the STIGS in the catalog path.
+	 */
 	public static ArrayList<String> readList(){
 		File catalogLocation = new File(catalogPath);
 		ArrayList<String> arrayOfSTIGS = enumerate(catalogLocation);
@@ -31,6 +39,10 @@ public class LocalCatalog {
 	}
 	
 	//takes in the converts the file, into a string thats readable to add to the arrayList of String which is the .xml files. Returns the array back.
+	/**
+	 * @param catalogLocation, takes in the catalog location to enumerate and create the array list of stigs.
+	 * @return, returns the array of STIGs after creating it from the given directory. 
+	 */
 	public static ArrayList<String> enumerate(File catalogLocation){
 		fileList = catalogLocation.list();
 		
@@ -44,11 +56,18 @@ public class LocalCatalog {
 	}
 	
 	// Takes in the .xml STIG file and deletes it from the directory and updates the arraylist and listview as well.
+	/**
+	 * @param stigFile, takes in the file.
+	 */
 	public static void deleteSTIGs(File stigFile) {
 		stigFile.delete();
 	}
 	
 	// Lets the user upload an .xml file from their local directory and checks if it was selected, if not then it outputs "ERROR: no file was selected".
+	/**
+	 * @param listView, takes in a list as a paramenter to update it.
+	 * @throws IOException, throws an exception if there is an error taking in the file.
+	 */
 	public static void uploadXML (ListView<String> listView) throws IOException {
 		FileChooser fc = new FileChooser();
     	
