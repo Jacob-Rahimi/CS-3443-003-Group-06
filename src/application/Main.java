640c5b55d8706409e57bb42b8131748e918cf597
Jikkou9 <72326079+Jacob-Rahimi@users.noreply.github.com>
2022-04-21 05:43:46 -0500

    Added Command Cyber icon for the application

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 
 
@@ -27,6 +28,7 @@ public class Main extends Application {
 			AnchorPane root = FXMLLoader.load(url);
 			Scene scene = new Scene(root,600,400);
 			primaryStage.setScene(scene);
+			primaryStage.getIcons().add(new Image("data/Command-Cyber-Logo.png"));
 			primaryStage.setTitle("Main Menu");
 			primaryStage.setResizable(false);
 			primaryStage.show();
